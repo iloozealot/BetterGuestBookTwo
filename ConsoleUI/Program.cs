3f45e4606c77f40882b7c4b6b7be14835b94e720
iloozealot <iloozealot@gmail.com>
2023-11-22 12:41:51 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using GuestBookTestLibrary;
+using GuestBookTestLibrary.Models;
+
+
+List<PersonModel> guestList = new List<PersonModel>();
+
+GenerateGuestList(guestList);
+
+foreach (PersonModel guest in guestList)
+{
+    Console.WriteLine($"{guest.FirstName} {guest.LastName} {guest.MessageToHost}");
+}
+
+
+
+
+Console.ReadLine();
+
+static void GenerateGuestList(List<PersonModel> guestList)
+{
+    string anotherGuest = "";
+
+    do
+    {
+        PersonModel person = new PersonModel();
+
+
+        Console.Write("What is your first name: ");
+        person.FirstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        person.LastName = Console.ReadLine();
+
+        Console.Write("What is your message to the host: ");
+        person.MessageToHost = Console.ReadLine();
+
+        Console.Write("Is there another guest (yes/no): ");
+        anotherGuest = Console.ReadLine();
+
+        guestList.Add(person);
+
+    } while (anotherGuest.ToLower() != "no");
+}
